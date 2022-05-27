edde23d056c4e9ec27c681ef4ed6cb49f1d36f91
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-27 16:53:49 -0400

    organize files

--- 2010 - 2011/Junior/Matrix Encryption/Main.java
+++ 2010 - 2011/Junior/Matrix Encryption/Main.java
@@ -1,43 +1,31 @@
 import java.io.*;
-import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
     try {
       BufferedReader reader = new BufferedReader(new FileReader("Test.in"));
       String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-      String line;
-      while ((line = reader.readLine()) != null) {
-        String code = line.substring(0, line.length()-8).replaceAll(",", "").trim();
-        String encodeMatrixStr = line.substring(line.length()-8).replaceAll(",", "").trim();
+      String input;
+      while ((input = reader.readLine()) != null) {
+        String code = input.substring(0, input.length()-8).replaceAll(",", "").trim();
+        String matrixEncoding = input.substring(input.length()-8).replaceAll(",", "").trim();
         if (code.length() % 2 == 1) code = code + " ";
 
-        int a = Character.getNumericValue(encodeMatrixStr.charAt(0));
-        int b = Character.getNumericValue(encodeMatrixStr.charAt(1));
-        int c = Character.getNumericValue(encodeMatrixStr.charAt(2));
-        int d = Character.getNumericValue(encodeMatrixStr.charAt(3));
-        int[][] matrix = new int[(int) (code.length()+1)/2][2];
-        // do matrix multiplication
-        // | a b |   | e |   | ae + bf |
-        // |     | x |   | = |         |
-        // | c d |   | f |   | ce + df |
-        for (int i = 0; i < matrix.length; i++) {
+        int a = Character.getNumericValue(matrixEncoding.charAt(0));
+        int b = Character.getNumericValue(matrixEncoding.charAt(1));
+        int c = Character.getNumericValue(matrixEncoding.charAt(2));
+        int d = Character.getNumericValue(matrixEncoding.charAt(3));
+
+        String output = "";
+        for (int i = 0; i < ((int) (code.length()+1)/2); i++) {
           int e = abc.indexOf(code.charAt(i*2))+1;
           int f = abc.indexOf(code.charAt(i*2+1))+1;
-          matrix[i][0] = (a*e) + (b*f);
-          matrix[i][1] = (c*e) + (d*f);
+          output = output + abc.charAt((((a*e) + (b*f))-1) % 27);
+          output = output + abc.charAt((((c*e) + (d*f))-1) % 27);
         }
-        
-        String output = "";
-        for (int i = 0; i < matrix.length; i++) {
-          for (int j = 0; j < matrix[i].length; j++) {
-            int num = matrix[i][j];
-            if (num > 27) num = num % 27;
-            if (num != 0) num = num - 1;
-            output = output + abc.charAt(num);
-          }
-        }
-        System.out.println(output + "\n");
+
+        System.out.printf("Input: %s\n", input);
+        System.out.printf("Output: %s\n\n", output);
       }
       reader.close();
     } catch(IOException e) {
