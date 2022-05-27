5d4b116687efad55db299a6ae6d23770be2d3326
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-27 00:02:05 -0400

    Update Main.java

--- 2010 - 2011/Junior/Matrix Encryption/Main.java
+++ 2010 - 2011/Junior/Matrix Encryption/Main.java
@@ -1,45 +1,31 @@
 import java.io.*;
+import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
     try {
       BufferedReader reader = new BufferedReader(new FileReader("Test.in"));
-      String abc = "abcdefghijklmnopqrstuvwxyz ";
+      String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
       String line;
       while ((line = reader.readLine()) != null) {
-        String code = line.trim().replaceAll(",", "").replaceAll("0", "").replaceAll("1", "").replaceAll("2", "").replaceAll("3", "").replaceAll("4", "").replaceAll("5", "").replaceAll("6", "").replaceAll("7", "").replaceAll("8", "").replaceAll("9", "");
-        String encodeMatrixStr = line.substring(line.length()-8).replaceAll(",", "").replace(" ", "");
-        if (code.length() % 2 == 0) code = code + " ";
-        System.out.println(code.trim().toUpperCase());
-        
-        int[] encodingMatrix = new int[4];
-        for (int i = 0; i < encodeMatrixStr.length(); i++) {
-          for (int j = 0; j < encodeMatrixStr.length(); j++) {
-            if (encodingMatrix[j] == 0) {
-              int num = Character.getNumericValue(encodeMatrixStr.charAt(i));
-              encodingMatrix[i] = num;
-              break;
-            }
-          }
-        }
-        
-        int[][] matrix = new int[(int) (code.length()/2 + (double) 0.5)][2];
+        String code = line.substring(0, line.length()-8).replaceAll(",", "").trim();
+        String encodeMatrixStr = line.substring(line.length()-8).replaceAll(",", "").trim();
+        if (code.length() % 2 == 1) code = code + " ";
+
+        int a = Character.getNumericValue(encodeMatrixStr.charAt(0));
+        int b = Character.getNumericValue(encodeMatrixStr.charAt(1));
+        int c = Character.getNumericValue(encodeMatrixStr.charAt(2));
+        int d = Character.getNumericValue(encodeMatrixStr.charAt(3));
+        int[][] matrix = new int[(int) (code.length()+1)/2][2];
         // do matrix multiplication
         // | a b |   | e |   | ae + bf |
         // |     | x |   | = |         |
         // | c d |   | f |   | ce + df |
-        int a = encodingMatrix[0];
-        int b = encodingMatrix[1];
-        int c = encodingMatrix[2];
-        int d = encodingMatrix[3];
-        for (int i = 0; i < code.trim().length(); i+=2) {
-          int first2dIndex = (int) i/2;
-          int e = abc.indexOf(code.toLowerCase().charAt(i))+1;
-          int f;
-          if (i+1 == code.length() && (i+1) % 2 == 1) f = 27;
-          else f = abc.indexOf(code.toLowerCase().charAt(i+1))+1;
-          matrix[first2dIndex][0] = (a*e) + (b*f);
-          matrix[first2dIndex][1] = (c*e) + (d*f);
+        for (int i = 0; i < matrix.length; i++) {
+          int e = abc.indexOf(code.charAt(i*2))+1;
+          int f = abc.indexOf(code.charAt(i*2+1))+1;
+          matrix[i][0] = (a*e) + (b*f);
+          matrix[i][1] = (c*e) + (d*f);
         }
         
         String output = "";
@@ -48,12 +34,10 @@ class Main {
             int num = matrix[i][j];
             if (num > 27) num = num % 27;
             if (num != 0) num = num - 1;
-            char letter = abc.charAt(num);
-            output = output + letter;
+            output = output + abc.charAt(num);
           }
         }
-
-        System.out.println(output.toUpperCase() + "\n");
+        System.out.println(output + "\n");
       }
       reader.close();
     } catch(IOException e) {
